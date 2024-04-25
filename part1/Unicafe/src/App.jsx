58a8291110e904902755b7183ced28c1ac37d435
jonahnlewis4 <jonahnlewis4@gmail.com>
2024-04-25 02:58:18 -0700

    complete exercise 1.11

--- part1/Unicafe/src/App.jsx
+++ part1/Unicafe/src/App.jsx
@@ -12,7 +12,10 @@ const Button = ({handleClick, text}) =>
 
 const StatisticLine = ({text, value}) => {
   return(
-    <div>{text} {value}</div>
+    <tr>
+      <td>{text}</td> 
+      <td>{value}</td>
+    </tr>
   )
 }
 
@@ -22,13 +25,16 @@ const Statistics = ({good, neutral, bad}) => {
       return (
         <div>  
           <h1>statistics</h1>
-          <StatisticLine text = "good" value = {good}/>
-          <StatisticLine text = "neutral" value = {neutral}/>
-          <StatisticLine text = "bad" value = {bad}/>
-          <StatisticLine text = "all" value = {good + neutral + bad}/>
-          <StatisticLine text = "average" value = {(good - bad)/(good + neutral + bad)}/>
-          <StatisticLine text = "positive" value = {(100 * good) / (good + neutral + bad) + ' %'}/>
-
+          <table>  
+            <tbody>
+              <StatisticLine text = "good" value = {good}/>
+              <StatisticLine text = "neutral" value = {neutral}/>
+              <StatisticLine text = "bad" value = {bad}/>
+              <StatisticLine text = "all" value = {good + neutral + bad}/>
+              <StatisticLine text = "average" value = {(good - bad)/(good + neutral + bad)}/>
+              <StatisticLine text = "positive" value = {(100 * good) / (good + neutral + bad) + ' %'}/>
+            </tbody>
+          </table>
         </div>
       )
     }
