b4d147f34671291e3a2320f7af5bcb395cb123c4
jonahnlewis4 <jonahnlewis4@gmail.com>
2024-04-26 02:46:49 -0700

    complete exercise 2.5

--- part2/courseInformation/src/App.jsx
+++ part2/courseInformation/src/App.jsx
@@ -1,3 +1,4 @@
+import Course from "./components/Course"
 const Header = (props) => {
   console.log(props)
   return (
@@ -7,43 +8,7 @@ const Header = (props) => {
   )
 }
 
-const Content = ({parts}) => {
-  console.log(parts)
-  return (
-  <div>   
-      
-    {parts.map( (part) => <Part part = {part}/>)}
-  </div>
-  )
-}
 
-const Part = ({part}) => {
-  console.log(part)
-  console.log(part.exercises)
-  return(
-      <p>{part.name} {part.exercises}</p>
-  )
-}
-
-const Total = ({parts}) => {
- return(
-    <strong>total of {parts.reduce((sum, part) => {return(sum + part.exercises)}, 0)} exercises</strong>
- )
-}
-
-const Course = ({course}) =>{
-  console.log(course)
-
-  return (
-    <div>
-      <h2>{course.name}</h2>
-    
-      <Content parts = {course.parts}/>
-      <Total parts = {course.parts}/>
-    </div>
-  
-  )
-}
 
 const App = () => {
   const courses = [
