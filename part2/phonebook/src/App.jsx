46c7f4a64afa4ec42f1ead4ec89c575fa1ba1a9e
jonahnlewis4 <jonahnlewis4@gmail.com>
2024-04-27 02:08:36 -0700

    complete exercises 2.9 and 2.10

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,11 +2,22 @@ import { useState } from 'react'
 
 const People = ({persons}) => {
   return persons.map(person => {
-    return <p key = {person.name + person.number}>{person.name} {person.number}</p>
+    return <p key = {person.id}>{person.name} {person.number}</p>
   })
 }
 
-const Form = ({onSubmit, name, number, onChanges}) =>{
+const SearchFilter = ({value, onChange}) => {
+  return (
+    <div>
+        filter shown with:   
+        <input  
+              value = {value}
+              onChange = {onChange}
+        />
+    </div>
+  )
+}
+const PersonForm = ({onSubmit, name, number, onChanges}) =>{
   return (
     <form onSubmit = {onSubmit}>
         <div>
@@ -32,18 +43,24 @@ const Form = ({onSubmit, name, number, onChanges}) =>{
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567'}
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNum, setNewNum] = useState('')
+  const [searchKey, setSearchKey] = useState('')
 
+  const [filteredPersons, setFilteredPersons] = useState(persons)
   const addPerson = (event) => {
     event.preventDefault()
     console.log('add number clicked')
   
     const newPerson = {
       name: newName,
-      number: newNum
+      number: newNum,
+      id: persons.length + 1
     }
 
     //if our persons array already contains the given person, do not add it to the array
@@ -68,14 +85,23 @@ const App = () => {
     console.log(event.target.value)
     setNewNum(event.target.value)
 }
+const handleSearchKeyChange = (event) => {
+  console.log(event.target.value)
+  setSearchKey(event.target.value)
+
+  setFilteredPersons(persons.filter(person => person.name.includes(event.target.value)))
+}
 
   return (
     <div>
       <h2>Phonebook</h2>
-
-      <Form onSubmit = {addPerson} name = {newName} number = {newNum} onChanges = {{handleNameChange, handleNumberChange}}/>
-      <h2>Numbers</h2>
-      <People persons = {persons}/>
+      
+      <SearchFilter value = {searchKey} onChange = {handleSearchKeyChange}/>
+      
+      <h3>add a new</h3>
+      <PersonForm onSubmit = {addPerson} name = {newName} number = {newNum} onChanges = {{handleNameChange, handleNumberChange}}/>
+      <h3>Numbers</h3>
+      <People persons = {filteredPersons}/>
     </div>
   )
 }
