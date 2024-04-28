28049ea19cc388c81ac453cd22406da41efb7a4c
jonahnlewis4 <jonahnlewis4@gmail.com>
2024-04-28 01:09:53 -0700

    take notes up to EX 2.12

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,7 +1,7 @@
 import Note from './components/Note'
 import {useState, useEffect} from 'react'
-import Axios from 'axios'
 import axios from 'axios'
+import noteService from './services/notes'
 
 
 
@@ -12,16 +12,17 @@ const App = (props) => {
   const [showAll, setShowAll] = useState(true)
   
   useEffect(() => {
-    console.log('effect')
-    axios 
-      .get('http://localhost:3001/notes')
-      .then(response=> {
-        console.log('promise fulfilled')
-        setNotes(response.data)
+    //console.log('effect')
+    noteService
+      .getAll()
+      .then( initialNotes=> {
+        //console.log('promise fulfilled')
+        setNotes(initialNotes)
       })
   }, [])
 
-  console.log('render', notes.length, 'notes')
+  //console.log(notes)
+  //console.log('render', notes.length, 'notes')
 
   const handleNoteChange = (event) => {
     console.log(event.target.value)
@@ -37,12 +38,39 @@ const App = (props) => {
     }
     
 
-    axios
-      .post('http://localhost:3001/notes', noteObject)
-      .then(response => {
-        setNotes(notes.concat(noteObject))
+      noteService.create(noteObject)
+      .then(newNote => {
+        console.log('posting to notes database')
+        setNotes(notes.concat(newNote))
         setNewNote('')
       })
+  }
+
+  const toggleImportanceOf = (id) => {
+    console.log(`importance of ${id} needs to be toggled`)  
+
+    
+    const note = notes.find(n => n.id === id)
+    const changedNote = {...note, important: !note.important}
+
+    //console.log(url, note, changedNote, id)
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(n => { 
+          console.log(n.id, id)
+          return n.id !== id ? n : returnedNote
+        }
+        ))
+      })
+      .catch(error => {
+        alert(
+          `the note '${note.content}' was already deleted from server`
+        )
+
+        setNotes(notes.filter(note => note.id != id))
+      })
+    
   }
   //if showALL is true, array of notes to show is all of em.
   //if show all is false, array of notes to show is only the important notes
@@ -57,9 +85,11 @@ const App = (props) => {
         </button>
       </div>
       <ul>
-        {console.log(notesToShow)}
         {notesToShow.map( (note) => 
-          <Note note = {note}/>
+          <Note 
+            key = {note.id} 
+            note = {note} 
+            toggleImportance = {() => toggleImportanceOf(note.id)}/>
         )}
       </ul>
       <form onSubmit = {addNote}>
