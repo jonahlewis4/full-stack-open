2cacada7c8ecbd5a3895fa0c408309c62483cdfe
jonahnlewis4 <jonahnlewis4@gmail.com>
2024-05-09 02:08:41 -0700

    add POST requests. (ex 3.6 compelte)

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+
+app.use(express.json())
+
 let persons = [
     { 
       "id": 1,
@@ -61,6 +64,51 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(person => person.id !== id)
 
     response.status(204).end()
+})
+
+const generateId = () => {
+    let id = Math.floor(Math.random() * 10000)
+    while(persons.some(person => person.id === id
+    )){
+        id = Math.floor(Math.random() * 10000)
+    }
+    return id
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    if(!body){
+        return response.status(400).json({
+            error: `unable to find request body`
+        })
+    }
+    else if(!body.name){
+        return response.status(400).json({
+            error: `name missing`
+        })
+    }
+    else if(!body.number){
+        return response.status(400).json({
+            error: `number missing`
+        })
+    }
+    //if there already exists someone with the same name in the place
+    else if(persons.some(person => person.name === body.name)){
+        //respond with an error
+        return response.status(403).json({
+            error: `name must be unique`
+        })
+    }
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
 })
 const PORT = 3001
 app.listen(PORT, () => {
