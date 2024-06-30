1a561258ff49a93a76ef0ae427c86cd52487be95
jonahnlewis4 <jonahnlewis4@gmail.com>
2024-06-29 20:40:30 -0700

    add esLint

--- part3/phonebook/backend/models/person.js
+++ part3/phonebook/backend/models/person.js
@@ -1,42 +1,42 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 require('dotenv').config()
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
-    .then(() => console.log('connected to the database'))
-    .catch((err) => console.log("error connecting to database" , err.message));
+  .then(() => console.log('connected to the database'))
+  .catch((err) => console.log('error connecting to database' , err.message))
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, "person's name required"],
-        minlength: [3, "Person's name must be at least 3 digits"]
+  name: {
+    type: String,
+    required: [true, 'person\'s name required'],
+    minlength: [3, 'Person\'s name must be at least 3 digits']
+  },
+
+  number: {
+    type: String,
+    validate: {
+      //validates if we have 2-3 digits followed by a dash followed by at least 1 digit.
+      validator: (value) => {
+        console.log('running validation for person\'s number', value)
+        const isValid =  /^\d{2,3}-\d+$/.test(value)
+        console.log(isValid)
+        return  isValid && value.length >= 8
+      },
+      message: props => `${props.value} is not a valid phone number! must be of from 2 to 3 digits followed by - and then at least 1 digit. Total length must be 8 or more`,
+
+
     },
+    required: [true, 'person\'s phone number required']
 
-    number: {
-        type: String,
-        validate: {
-            //validates if we have 2-3 digits followed by a dash followed by at least 1 digit.
-            validator: (value) => {
-                console.log("running validation for person's number", value)
-                const isValid =  /^\d{2,3}-\d+$/.test(value)
-                console.log(isValid)
-                return  isValid && value.length >= 8
-            },
-            message: props => `${props.value} is not a valid phone number! must be of from 2 to 3 digits followed by - and then at least 1 digit. Total length must be 8 or more`,
-
-
-        },
-        required: [true, "person's phone number required"]
-
-    }
+  }
 })
 personSchema.set('toJSON', {
-    transform: (doc, ret) => {
-        ret.id = ret._id.toString()
-        delete ret._id
-        delete ret.__v
-    }
+  transform: (doc, ret) => {
+    ret.id = ret._id.toString()
+    delete ret._id
+    delete ret.__v
+  }
 })
-module.exports = mongoose.model("Person", personSchema)
+module.exports = mongoose.model('Person', personSchema)
--- part3/phonebook/backend/mongo.js
+++ part3/phonebook/backend/mongo.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose')
 
 if(process.argv.length < 3){
-    console.log('give password, name, number as arguments')
-    console.log('give only the password to see entire database')
-    process.exit(1)
-
+  console.log('give password, name, number as arguments')
+  console.log('give only the password to see entire database')
+  process.exit(1)
 }
 const password = process.argv[2]
 const url = `mongodb+srv://jonahnlewis4:${password}@cluster0.o3hotgu.mongodb.net/phoneBook?retryWrites=true&w=majority&appName=Cluster0`
@@ -17,8 +16,8 @@ mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+  name: String,
+  number: String,
 })
 
 const Person = mongoose.model('Person', personSchema)
@@ -33,22 +32,22 @@ const Person = mongoose.model('Person', personSchema)
 //     mongoose.connection.close()
 // })
 if(process.argv.length === 5) {
-    const person = new Person({
-        name: passedName,
-        number: passedNumber,
-    })
-    person.save().then(result => {
-        console.log("added " + person.name + " number " + person.number + " to the phonebook")
-        mongoose.connection.close()
-    })
+  const person = new Person({
+    name: passedName,
+    number: passedNumber,
+  })
+  person.save().then(() => {
+    console.log('added ' + person.name + ' number ' + person.number + ' to the phonebook')
+    mongoose.connection.close()
+  })
 }
 
 
 if(process.argv.length === 3) {
-    Person.find({}).then(result => {
-        result.forEach(person => {
-            console.log(person)
-        })
-        mongoose.connection.close()
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person)
     })
+    mongoose.connection.close()
+  })
 }
